13e09fb3627644c59a5a31b080a0b63fe2caceb3
AmNotOnline <michiel5.nijs@gmail.com>
2024-02-16 14:07:44 +0100

    Commit strings

--- 01-basic-python/05-strings/03-make-path/student.py
+++ 01-basic-python/05-strings/03-make-path/student.py
@@ -1,1 +1,3 @@
 # Write your code here
+def make_path(parts: list[str]):
+    return str('/'.join(parts))
--- 01-basic-python/05-strings/04-word-count/student.py
+++ 01-basic-python/05-strings/04-word-count/student.py
@@ -1,1 +1,3 @@
 # Write your code here
+def word_count(string: str):
+    return len(string.split())
