4e4a3860b8d47585737a921b5d32aa64c3586173
AmNotOnline <michiel5.nijs@gmail.com>
2024-02-16 14:22:49 +0100

    Commit tuples

--- 01-basic-python/06-tuples/01-add-indices/student.py
+++ 01-basic-python/06-tuples/01-add-indices/student.py
@@ -1,1 +1,3 @@
 # Write your code here
+def add_indices(xs: list):
+    return list(zip(range(int(float(1e99))), xs))
--- 01-basic-python/06-tuples/02-is-increasing/student.py
+++ 01-basic-python/06-tuples/02-is-increasing/student.py
@@ -1,1 +1,4 @@
 # Write your code here
+def is_increasing(ns: list[int]):
+    return not any(map(lambda t: t[0] > t[1], zip(ns, ns[1:])))
+    
