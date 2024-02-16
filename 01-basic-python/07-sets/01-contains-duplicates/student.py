19e36fbe0215f572ef2711f842149b147854d6c9
AmNotOnline <michiel5.nijs@gmail.com>
2024-02-16 14:45:15 +0100

    Commit sets

--- 01-basic-python/07-sets/01-contains-duplicates/student.py
+++ 01-basic-python/07-sets/01-contains-duplicates/student.py
@@ -1,1 +1,3 @@
 # Write your code here
+def contains_duplicates(ns: list):
+    return len(ns) != len(set(ns))
--- 01-basic-python/07-sets/02-remove-duplicates/student.py
+++ 01-basic-python/07-sets/02-remove-duplicates/student.py
@@ -1,1 +1,10 @@
 # Write your code here
+def remove_duplicates(xs: list):
+    unq = set()
+    out = []
+    for i in xs:
+        if i not in unq:
+            out.append(i)
+            unq.add(i)
+    
+    return out
