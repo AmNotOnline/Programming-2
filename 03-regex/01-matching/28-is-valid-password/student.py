c3533b0c170b68301488cde28bd917bde223f909
AmNotOnline <michiel5.nijs@gmail.com>
2024-03-15 14:47:54 +0100

    Password validation for real this time

--- 03-regex/01-matching/28-is-valid-password/student.py
+++ 03-regex/01-matching/28-is-valid-password/student.py
@@ -1,11 +1,7 @@
 # Write your code here
 import re
 def is_valid_password(string: str):
-    return len(string) >= 12 \
-        and re.search(r'\d', string) \
-        and re.search(r'[a-z]', string) \
-        and re.search(r'[A-Z]', string) \
-        and re.search(r'[+\-*/.@]', string) \
-        and not re.search(r'(.)\1\1', string) \
-        and not re.search(r'(.)*.\1*.\1*.\1', string)
-            
+    return re.fullmatch(
+            r'(?=.*\d)(?=.*[A-Z])(?=.*[a-z])(?=.*[+\-*/.@])(?!.*(.)\1\1)(?!.*(.)(.*\2){3,}).{12,}',
+            string
+        ) 
